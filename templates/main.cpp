61c2148a8c1d30bd7eb2a8e38efab62d48380efc
Ranjith Hegde <ranjithshegde@gmail.com>
2026-02-02 01:59:34 +0530

    bump template version with macros

--- templates/main.cpp
+++ templates/main.cpp
@@ -34,7 +34,8 @@ int main()
 {
     try {
         MF_PRINT(MayaFlux::Journal::Component::USER, MayaFlux::Journal::Context::Init, "=== MayaFlux Creative Coding Framework ===");
-        MF_PRINT(MayaFlux::Journal::Component::USER, MayaFlux::Journal::Context::Init, "Version: {}", "0.1.1");
+        MF_PRINT(MayaFlux::Journal::Component::USER, MayaFlux::Journal::Context::Init,
+                 "Version: {}.{}.{}", MAYAFLUX_VERSION_MAJOR, MAYAFLUX_VERSION_MINOR, MAYAFLUX_VERSION_PATCH);
         MF_PRINT(MayaFlux::Journal::Component::USER, MayaFlux::Journal::Context::Init, "");
 
         initialize();
